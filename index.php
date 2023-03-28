8b37c731b81fdb82fa44ea060a25fdf85395296e
Shabie Hassan <shabeeulhassan40@gmail.com>
2023-03-28 16:09:01 +0500

    initialzed project

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+
+	<head>
+		<link rel="stylesheet" href="assets/css/styles.css" /> 
+	</head>
+
+	
+	<body>
+		<div class="container">
+			<div class="mignon-container">
+				<div class="mignon-dashboard">
+					<div class="mignon-tech">
+						<img src="assets/images/mignon.svg" />
+						<p>Mignonify</p>
+					</div>
+
+					<div>
+						
+						
+					</div>
+				
+				</div>
+
+				<div class="analytics">
+					
+				</div>
+			</div>
+
+
+			
+		</div>
+		<script src="https://d3js.org/d3.v7.min.js"></script>
+	</body>
+
+</html>
+
+
+
